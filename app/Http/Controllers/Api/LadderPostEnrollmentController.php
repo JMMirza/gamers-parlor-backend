946634b05b539bbd159dda184fc71aea2afcf087
jmmirza <janmuhammadmirza@gmail.com>
2022-12-23 11:43:56 +0500

    changes

--- app/Http/Controllers/Api/LadderPostEnrollmentController.php
+++ app/Http/Controllers/Api/LadderPostEnrollmentController.php
@@ -16,23 +16,28 @@ class LadderPostEnrollmentController extends Controller
             'team_id' => 'required|integer',
             // 'request_time' => 'required',
         ]);
-        // $ladder_post  = WagerPost::find($request->ladder_post_id);
-        // $ladder_post->status = 'PENDING';
-        $input  = $request->all();
-        $input['status'] = 'PENDING';
+        $ladder_post = LadderPost::where('id', $request->ladder_post_id)->first();
+        if (Auth::user()->balance >= $ladder_post->fee) {
+            $input  = $request->all();
+            $input['status'] = 'ACCEPTED';
 
-        $request = LadderPostEnrollment::create($input);
-        $data = [
-            'request' => $request
-        ];
-        return response($data, 200);
+            $request = LadderPostEnrollment::create($input);
+            $ladder_post->status = 'On going';
+            $ladder_post->save();
+            $data = [
+                'request' => $request
+            ];
+            return response($data, 200);
+        }
+        return response(['message' => 'Not Enough Credits'], 200);
     }
 
     public function getLadderPostEnrollment(Request $request)
     {
         $ladder_requests = LadderPostEnrollment::where('ladder_post_id', $request->ladder_post_id)
-            ->where('status', 'PENDING')
-            ->with(['team', 'ladder_post.game', 'ladder_post.host'])->get();
+            ->with(['team.team_members', 'ladder_post.game', 'ladder_post.host'])
+            ->where('status', 'ACCEPTED')
+            ->first();
         return response($ladder_requests, 200);
     }
 
